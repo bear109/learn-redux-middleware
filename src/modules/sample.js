66c9a0b3a30370e6cc641464cb72d945b23b3e4d
iwinix109 <iwinix109@naver.com>
2021-07-17 15:25:46 +0900

    redux-thunk-final

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -4,33 +4,20 @@ import createRequestThunk from '../lib/createRequestThunk';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 export const getPost = createRequestThunk(GET_POST, api.getPost);
 export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
-  loading: {
-    GET_POST: false,
-    GET_USERS: false,
-  },
   post: null,
   users: null,
 };
 
 const sample = handleActions(
   {
-    [GET_POST]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true,
-      },
-    }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
       loading: {
@@ -39,20 +26,6 @@ const sample = handleActions(
       },
       post: action.payload,
     }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
-    }),
-    [GET_USERS]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true,
-      },
-    }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
       loading: {
@@ -60,13 +33,6 @@ const sample = handleActions(
         GET_POST: false,
       },
       post: action.payload,
-    }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
     }),
   },
   initialState
